5e93daf4f70bd11b0d67de87fe405c34213e64b9
Marco Giacomello <m.giacomello@insiel.it>
2022-11-18 20:31:56 +0100

    first commit

---
+++ src/main/java/com/marco/ultimabreweryservice/UltimaBreweryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marco.ultimabreweryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimaBreweryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltimaBreweryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marco/ultimabreweryservice/UltimaBreweryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.ultimabreweryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimaBreweryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
