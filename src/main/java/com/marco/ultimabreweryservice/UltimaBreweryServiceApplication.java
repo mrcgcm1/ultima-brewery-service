721dfb0671556e1fc4f7fda90584eef1d71c0652
Marco Giacomello <m.giacomello@insiel.it>
2022-12-09 22:21:47 +0100

    inserito jmsConfig

--- src/main/java/com/marco/ultimabreweryservice/UltimaBreweryServiceApplication.java
+++ src/main/java/com/marco/ultimabreweryservice/UltimaBreweryServiceApplication.java
@@ -1,11 +1,14 @@
 package com.marco.ultimabreweryservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
+@SpringBootApplication()
 public class UltimaBreweryServiceApplication {
 
     public static void main(String[] args) {
