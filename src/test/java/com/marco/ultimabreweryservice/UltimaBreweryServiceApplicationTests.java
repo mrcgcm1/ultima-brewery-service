4997df0d131273fbf354a9c55ed46ee0987cb3c8
Marco Giacomello <m.giacomello@insiel.it>
2023-01-12 22:33:32 +0100

    @wip

--- src/test/java/com/marco/ultimabreweryservice/UltimaBreweryServiceApplicationTests.java
+++ src/test/java/com/marco/ultimabreweryservice/UltimaBreweryServiceApplicationTests.java
@@ -1,12 +1,11 @@
 package com.marco.ultimabreweryservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UltimaBreweryServiceApplicationTests {
 
-    @Test
+ //   @Test
     void contextLoads() {
     }
 
